12172ad98a253326316f8f33aa526167ed92f374
TheDragonSkull <sergibot6000@gmail.com>
2025-06-12 12:16:25 +0200

    Adding new Bear Trap

--- src/main/java/net/thedragonskull/trapsmod/block/custom/BearTrap.java
+++ src/main/java/net/thedragonskull/trapsmod/block/custom/BearTrap.java
@@ -1,38 +1,49 @@
 package net.thedragonskull.trapsmod.block.custom;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.thedragonskull.trapsmod.block.entity.BearTrapBE;
 import net.thedragonskull.trapsmod.sound.ModSounds;
 import org.jetbrains.annotations.Nullable;
 
+import static net.thedragonskull.trapsmod.util.BearTrapUtils.rotateShapeFrom;
+
 public class BearTrap extends BaseEntityBlock {
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
     public static final BooleanProperty TRAP_SET = BooleanProperty.create("trap_set");
+
+    protected static final VoxelShape SET_SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 5.0D, 16.0D);
+    protected static final VoxelShape CLOSED_SHAPE = Block.box(4.0D, 0.0D, 0.0D, 12.0D, 9.0D, 16.0D);
 
     public BearTrap(Properties pProperties) {
         super(pProperties);
         this.registerDefaultState(
                 this.stateDefinition.any()
+                        .setValue(FACING, Direction.NORTH)
                         .setValue(TRAP_SET, false));
     }
 
     @Override
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(TRAP_SET);
+        pBuilder.add(FACING, TRAP_SET);
     }
 
     @Override
@@ -66,13 +77,44 @@ public class BearTrap extends BaseEntityBlock {
     }
 
     @Override
-    public boolean propagatesSkylightDown(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
-        return true;
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState()
+                .setValue(FACING, context.getHorizontalDirection().getOpposite())
+                .setValue(TRAP_SET, false);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction direction = state.getValue(FACING);
+        boolean isTrapSet = state.getValue(TRAP_SET);
+        VoxelShape shape = isTrapSet ? SET_SHAPE : CLOSED_SHAPE;
+
+        return rotateShapeFrom(Direction.NORTH, direction, shape);
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        Direction direction = state.getValue(FACING);
+        boolean isTrapSet = state.getValue(TRAP_SET);
+        VoxelShape shape = isTrapSet ? SET_SHAPE : CLOSED_SHAPE;
+
+        return rotateShapeFrom(Direction.NORTH, direction, shape);
     }
 
     @Override
     public RenderShape getRenderShape(BlockState pState) {
         return RenderShape.ENTITYBLOCK_ANIMATED;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rotation) {
+        return state.setValue(FACING, rotation.rotate(state.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirror) {
+        Rotation rotation = mirror.getRotation(state.getValue(FACING));
+        return this.rotate(state, null, null, rotation);
     }
 
     @Override
